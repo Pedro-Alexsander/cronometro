c363cb8cf7fb451c1558e8aef9e061a2ea3717f7
Pedro Alexsander <pedroalexsander22@gmail.com>
2022-03-08 18:34:13 -0300

    verificação da inicialização

--- script.js
+++ script.js
@@ -3,8 +3,12 @@ let seg = 0;
 let mili = 0;
 let tempo = 1000/60;
 let cron;
+let isVerificar = false
+
+
 
 function iniciar(){
+    if(isVerificar == false){
     cron = setInterval(function(){
         mili++;
         if(mili == 60){
@@ -21,15 +25,18 @@ function iniciar(){
         let formato = (min<10 ? "0"+ min : min) + ":" + (seg<10 ? "0" + seg : seg) + ":" + ( mili < 10 ? "0" + mili : mili)
         document.getElementById("formato").innerText = formato;
     },tempo)
+    isVerificar = true}
 }
 
 function parar(){
     clearInterval(cron)
+    isVerificar = false
 }
 function zerar(){
     clearInterval(cron)
      min = 0;
     seg = 0;
     mili = 0;
+    isVerificar = false
     document.getElementById("formato").innerText = "00:00:00"
 }
